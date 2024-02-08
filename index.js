5dfe85d6685fba49787ecabc5bad05d718305cf9
SakshiMSawant <134490848+SakshiMSawant@users.noreply.github.com>
2024-02-08 10:22:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Q.1
+let a = "Sakshi";
+let b = 6;
+console.log(a + b);
+
+// Q.2
+console.log(typeof (a + b));
+
+// Q.3
+const a1 = {
+  name: "Sakshi",
+  section: 1,
+  isPrincipal: false,
+};
+
+// Q.4
+a1["friend"] = "Nikita";
+a1["name"] = "Pragati";
+console.log(a1);
+
+// Q.5
+const dict = {
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work.",
+};
+
+console.log(dict["appreciate"]);
